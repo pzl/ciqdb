b6a2a2b714558ef3c7b68db43e3f0ccb6efa9560
pzl <dan@panzarel.la>
2018-06-14 22:02:42 -0400

    stub for eventual symboltable parsing

---
+++ parse/symboltable.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+
+)
+
+/*
+	
+	TYPES:
+		0: default?
+		1: method (+ nativemethod)
+		2: classdef
+		4: moduledef
+		8: int,float,string,null
+
+
+	01               TYPE
+	00 05            LENGTH (5)
+	73 74 61 72 74   VALUE (start)
+
+
+*/
